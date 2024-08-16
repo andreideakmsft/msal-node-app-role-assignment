052209a5375465ae65f27deca693fa318f9f943b
Andrei Deak <andrei.deak@microsoft.com>
2024-08-16 09:35:23 +0200

    update comments in index.ts

--- index.ts
+++ index.ts
@@ -12,27 +12,29 @@ const credential = new ClientSecretCredential(
     `${process.env.TENANT_ID}`,
     `${process.env.CLIENT_ID}`,
     `${process.env.CLIENT_SECRET}`,
-  );
+);
   
-  // @microsoft/microsoft-graph-client/authProviders/azureTokenCredentials
-  const authProvider = new TokenCredentialAuthenticationProvider(credential, {
-    // The client credentials flow requires that you request the
-    // /.default scope, and pre-configure your permissions on the
-    // app registration in Azure. An administrator must grant consent
-    // to those permissions beforehand.
+// @microsoft/microsoft-graph-client/authProviders/azureTokenCredentials
+const authProvider = new TokenCredentialAuthenticationProvider(credential, {
+// The client credentials flow requires that you request the
+// /.default scope, and pre-configure your permissions on the
+// app registration in Azure. An administrator must grant consent
+// to those permissions beforehand.
     scopes: ['https://graph.microsoft.com/.default'],
-  });
-  
-  const graphClient = Client.initWithMiddleware({ authProvider: authProvider });
+});
 
+// Instantiate Graph Client
+const graphClient = Client.initWithMiddleware({ authProvider: authProvider });
 
+// Define the App Role Assignment object
 const appRoleAssignment = {
     principalId: 'da55e018-55f8-48d3-aa49-7695f104d1ba', // Object ID of the Service Principal which will be granted the App Role
     resourceId: '26444935-9f63-403b-916d-55b67fefa6ab', // Object ID of the Service Principal exposing the App Role
     appRoleId: '5dc9486f-0607-4907-a159-7b8e5d819115' // ID of the App Role
-  };
-  
-const resp = await graphClient.api('/servicePrincipals/26444935-9f63-403b-916d-55b67fefa6ab/appRoleAssignedTo')
-    .post(appRoleAssignment);
+};
+
+// Call Microsoft Graph to create the App Role Assignment
+const resp = await graphClient.api('/servicePrincipals/26444935-9f63-403b-916d-55b67fefa6ab/appRoleAssignedTo').post(appRoleAssignment);
+
 
 console.log(resp);
